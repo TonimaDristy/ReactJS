058a84463721f5d70b56e0f4068304c75871025a
TonimaDristy <tidristy477@gmail.com>
2025-12-08 01:41:33 +0600

    tailwind.config.js

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,13 +1,21 @@
-<main>
-  <div className="relative">
-    <div className="pattern"></div>
+import React from "react";
 
-    <div className="wrapper">
-      <header>
-        <h1>
-          Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
-        </h1>
-      </header>
-    </div>
-  </div>
-</main>
+export default function App() {
+  return (
+    <main>
+      <div className="relative">
+        {/* Background Pattern */}
+        <div className="pattern"></div>
+
+        {/* Wrapper Content */}
+        <div className="wrapper">
+          <header>
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
+            </h1>
+          </header>
+        </div>
+      </div>
+    </main>
+  );
+}
