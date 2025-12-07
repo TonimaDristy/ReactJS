de54a462cc4a3d36df85d3d3a01a7b2f68efd643
TonimaDristy <tidristy477@gmail.com>
2025-12-08 02:00:19 +0600

    text

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,20 +1,15 @@
-import React from "react";
-
-export default function App() {
+const App = () => {
   return (
     <main>
-      <div className="relative">
-        {/* Background Pattern */}
-        <div className="pattern"></div>
-
-        {/* Wrapper Content */}
-        <div className="wrapper">
-          <header>
-            <h1>
-              Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
-            </h1>
-          </header>
-        </div>
+      <div className="pattern"></div>
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+        <p>Search</p>
       </div>
     </main>
   );
