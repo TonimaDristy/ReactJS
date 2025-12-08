0258b92a7912ef60063828765257826968b9ae31
TonimaDristy <tidristy477@gmail.com>
2025-12-09 00:25:59 +0600

    css4

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <div>
 
 
-      <h1 className="text-3xl font-bold underline text-red-500">
+      <h1 className="text-3xl font-bold underline ">
         Hello world!
       </h1>
 
--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";   // <-- THIS IS REQUIRED (very important)
+//import "./index.css";   // <-- THIS IS REQUIRED (very important)
+import './tailwind.css';
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
