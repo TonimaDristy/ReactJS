da88003f8ffc4ba070e94c25610652c754467d90
TonimaDristy <tidristy477@gmail.com>
2025-12-09 00:10:45 +0600

    tailwind

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
     <div>
 
 
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold underline text-red-500">
         Hello world!
       </h1>
 
