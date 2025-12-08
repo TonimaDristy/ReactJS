d06ccc292740747f3e516eff90598cdc32a5a1d5
TonimaDristy <tidristy477@gmail.com>
2025-12-08 23:07:17 +0600

    new

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,16 +1,21 @@
+import React from 'react'
+
 const App = () => {
   return (
-    <main>
-      <div className="pattern"></div>
-      <div className="wrapper">
-        <header>
-          <h1>
-            Find <span className="text-gradient">Movies</span> You'll Enjoy
-            Without the Hassle
-          </h1>
-        </header>
-        <p>Search</p>
-      </div>
-    </main>
-  );
+
+
+    <div>
+
+
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+
+
+    </div>
+
+  )
 }
+
+export default App
