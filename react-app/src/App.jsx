e6f69944ce13ff150d72792938a30f05621dab06
TonimaDristy <tidristy477@gmail.com>
2025-12-12 03:26:15 +0600

    newheadline

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -89,7 +89,7 @@ const App = () => {
 
           <img src="./hero.png" alt="Hero Banner" />
           <h1>
-            Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
+            Discover Your Next Favorite <span className="text-gradient">Movies</span> Effortlessly
           </h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           <h2 className="text-white">{searchTerm}</h2>
